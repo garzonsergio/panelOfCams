db103c511c1eb80b2d1c197d42ab753c4aeb2cbf
Sergio C Garzon <scgarzonp@gmail.com>
2022-03-30 17:38:07 -0500

    footer icons included

--- src/DcpFooter/index.js
+++ src/DcpFooter/index.js
@@ -1,16 +1,38 @@
 import React from "react";
 import "./DcpFooter.css";
+import { socialMedia } from "./socialMedia.js";
 
 function DcpFooter() {
   return (
     <React.Fragment>
       <footer>
+        <div className="social-media-container">
+          <span>
+            {socialMedia.map((media) => (
+              <a
+                className="media-logo-container"
+                key={media.name}
+                href={media.link}
+              >
+                <img className="mediaImage" src={media.icon} />
+              </a>
+            ))}
+          </span>
+          <div className="twitterUsers">
+            <p>@areametropol</p>
+            <p>@siatamedellin</p>
+          </div>
+        </div>
+        <div className="websites-container">
+          <span>
+            <img src="#" />
+            <p>www.metropol.gov.co</p>
+          </span>
+          <p>www.siata.gov.co</p>
+        </div>
         <span>
-          <p>Footer con derechos de autor etc</p>
+          <p>Desarrollado por SIATA I 2022 - 2025</p>
         </span>
-        <figure>
-          <img src="#" alt="footer-logo" />
-        </figure>
       </footer>
     </React.Fragment>
   );
---
+++ src/DcpFooter/socialMedia.js
@@ -0,0 +1,28 @@
+import facebook from "../assets/img/facebook.svg";
+import instagram from "../assets/img/instagram.svg";
+import twitter from "../assets/img/twitter.svg";
+import youtube from "../assets/img/youtube.svg";
+const socialMedia = [
+  {
+    name: "facebook",
+    link: "https://www.facebook.com/siatamedellin/",
+    icon: facebook,
+  },
+  {
+    name: "twitter",
+    link: "https://twitter.com/siatamedellin?lang=es",
+    icon: twitter,
+  },
+  {
+    name: "instagram",
+    link: "https://www.instagram.com/siatamedellin/",
+    icon: instagram,
+  },
+  {
+    name: "youtube",
+    link: "https://www.youtube.com/user/siatamedellin",
+    icon: youtube,
+  },
+];
+
+export { socialMedia };
