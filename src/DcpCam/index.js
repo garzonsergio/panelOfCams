fd1148bca22aa115db1cd1d47654cde7f5d00801
Sergio C Garzon <scgarzonp@gmail.com>
2022-04-07 15:58:37 -0500

    Automatic Reload of DcCams with timeInterval

--- src/DcpCam/index.js
+++ src/DcpCam/index.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./DcpCam.css";
 
 function DcpCam(props) {
+  const [reload, setReload] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setReload(() => Date.now());
+    }, 1000);
+  }, []);
+
   return (
     <React.Fragment>
       <a href={props.route} target="_blank">
-        <img src={props.route} alt={props.route} />
+        <img src={`${props.route}?${reload}`} alt={props.route} />
       </a>
     </React.Fragment>
   );
--- src/DcpCameraGrid/ListOfCams.js
+++ src/DcpCameraGrid/ListOfCams.js
@@ -138,6 +138,7 @@ function camsDetails(array) {
       typeOfCam: typeOfCams(array[i]),
     });
   }
+
   return cams;
 }
 
